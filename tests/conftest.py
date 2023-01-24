6cb2cdfe10eea8fdb74054cc4de75f0de0baff4b
cherrydan <cherrydan.1978@gmail.com>
2023-01-24 21:36:37 +0300

    complete conftest file

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,3 +14,42 @@ INSERT INTO USERS (username, is_deleted) VALUES(%s, %s) RETURNING user_id;
 GET_USER_BY_ID_QUERY = """
 SELECT * FROM users WHERE user_id = %s;
 """
+
+
+@pytest.fixture(scope="session", autouse=True)
+def create_database_and_run_migrations():
+    print("Running migrations...")
+    os.system(f"yoyo apply --database {DATABASE_URL} ./migrations -b")
+
+
+@pytest.fixture(scope="session")
+def test_client():
+    with TestClient(create_app()) as client:
+        yield client
+
+
+@pytest.fixture(scope="function", autouse=True)
+def clean_tables(test_client):
+    cursor = test_client.app.state.db.cursor()
+    cursor.execute("""TRUNCATE TABLE users; """)
+    test_client.app.state.db.commit()
+
+
+def create_test_user_in_db(username: str, db_connection, is_deleted: bool = False, user_id: int=None) -> int:
+    cursor = db_connection.cursor()
+    if user_id is None:
+        cursor.execute(CREATE_USER_QUERY_COMMON, (username, is_deleted))
+    else:
+        cursor.execute(CREATE_USER_QUERY_COMMON, (user_id, username, is_deleted))
+    return cursor.fetchone()[0]
+
+
+def get_user_from_db_by_id(user_id: int, db_connection):
+    cursor = db_connection.cursor()
+    cursor.execute(GET_USER_BY_ID_QUERY, (user_id, ))
+    return cursor.fetchone()
+
+
+
+
+
